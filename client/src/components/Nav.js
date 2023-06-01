671ce11d252dcf99a544ceda5fcb83c97ed104eb
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-01 16:48:22 +0600

    Modal

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { CiUser } from "react-icons/ci";
 import { RiSearchLine } from "react-icons/ri";
 import { TiTick } from "react-icons/ti";
+import Modal from "./Modal";
 
 export default function Nav() {
+    const [modal, setModal] = useState(false);
   return (
     <>
       <nav className="shad rounded-lg py-3 px-5 flex justify-between items-center">
@@ -17,13 +20,13 @@ export default function Nav() {
           <div className="relative">
             <input
               type="text"
-              className="bg-gray-200 rounded-md py-2 w-96 px-10"
+              className="bg-gray-200 rounded-md py-2 w-96 px-10 "
               placeholder="Search"
             />
             <RiSearchLine className="text-gray-400 text-2xl absolute left-2 top-2" />
           </div>
           <div>
-            <button className="px-5 mt-1.5" >
+            <button className="px-5 mt-1.5" onClick={() => setModal(true)}>
               <AiOutlinePlus className="text-2xl text-gray-600" />
             </button>
           </div>
@@ -35,6 +38,7 @@ export default function Nav() {
         </div>
       </nav>
       <div>
+        {modal && <Modal view={modal} setModal={setModal} />}
       </div>
     </>
   );
