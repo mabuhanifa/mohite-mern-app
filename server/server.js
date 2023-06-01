4721ea5d80fa198617c0bebc93b164eed86e9920
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-01 16:01:28 +0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(` Server is running on port ${port}... `);
+});
