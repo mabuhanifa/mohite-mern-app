cb113776c048d643698956b87231d4cc1ed4fd0c
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-02 12:14:30 +0600

    react toast & calendar

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import { VscChromeClose } from "react-icons/vsc";
 import { useDispatch, useSelector } from "react-redux";
 import { addTask } from "../redux/slices/taskSlice";
 
 export default function Modal({ view, setModal }) {
+  const [value, onChange] = useState(new Date());
   const dispatch = useDispatch();
   const { tasks } = useSelector((state) => state);
 
-  console.log(tasks);
+  console.log(value);
 
   const [title, setTitle] = useState("");
   if (!view) {
@@ -30,7 +33,7 @@ export default function Modal({ view, setModal }) {
   return (
     <div
       id="container"
-      className="mx-5 bg-opacity-30 backdrop-blur fixed inset-0 flex justify-center z-15 py-10"
+      className="bg-opacity-30 backdrop-blur fixed inset-0 flex justify-center z-15 py-10"
       onClick={closeModal}
     >
       <div className="max-w-2xl rounded-xl p-10 relative shad h-max">
@@ -66,6 +69,9 @@ export default function Modal({ view, setModal }) {
             placeholder="Priority"
             className="p-2 bg-slate-200 w-96 my-5"
           />
+          <div className="flex justify-center">
+            <Calendar onChange={onChange} value={value} />
+          </div>
           <button
             className="bg-blue-500 px-10 text-white p-2 rounded w-full mt-10"
             onClick={createTodo}
