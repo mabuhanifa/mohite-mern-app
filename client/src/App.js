3cbb73a8305b2f72dc21707a1a24c018fe55f50e
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-02 17:18:11 +0600

    routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Completed from "./components/Completed";
 import Dashboard from "./components/Dashboard";
 import Layout from "./components/Layout";
+import Pending from "./components/Pending";
+import Today from "./components/Today";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/pending" element={<Pending />} />
+        <Route path="/today" element={<Today />} />
+        <Route path="/finished" element={<Completed />} />
       </Routes>
     </Layout>
   );
--- client/src/components/Pending.js
+++ client/src/components/Pending.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { useGetTasksQuery } from '../redux/slices/apiSlice';
+import Task from './Task';
 
 export default function Pending() {
-  return (
-    <div>Pending</div>
-  )
+    const { data: tasks, isError, isLoading, isSuccess } = useGetTasksQuery();
+    return (
+      <div>
+        <Toaster />
+        {isLoading && <h1>Loading...</h1>}
+        {isError && <h1>Error...</h1>}
+        <div>
+          {isSuccess && tasks.map((task) => <Task key={task._id} task={task} />)}
+        </div>
+      </div>
+    );
 }
