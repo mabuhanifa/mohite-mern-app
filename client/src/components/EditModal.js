3c5c41c5d4562b7eea33268e58c6427d54b1768f
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-02 17:26:47 +0600

    edit modal

---
+++ client/src/components/EditModal.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import toast, { Toaster } from "react-hot-toast";
+import { VscChromeClose } from 'react-icons/vsc';
+import { useAddTaskMutation } from '../redux/slices/apiSlice';
+
+export default function EditModal({ view, setModal }) {
+    const [selectedOption, setSelectedOption] = useState("pending");
+    const [date, setDate] = useState("");
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+  
+    const [addTask] = useAddTaskMutation();
+  
+    const options = [
+      { value: "completed", label: "Completed" },
+      { value: "pending", label: "Pending" },
+    ];
+  
+    const handleChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+  
+    // const dispatch = useDispatch();
+  
+    if (!view) {
+      return null;
+    }
+    const closeModal = (e) => {
+      if (e.target.id === "container") {
+        setModal((m) => !m);
+      }
+    };
+  
+    const createTodo = (e) => {
+      e.preventDefault();
+      addTask({
+        title,
+        description,
+        status: selectedOption,
+        date,
+      }).then((res) => {
+        if (res.data.status === "success") {
+          toast.success("Task added successfully", { duration: 2500 });
+          setTimeout(() => {
+            setModal((m) => !m);
+          }, 1000);
+        }
+      });
+    };
+  return (
+    <>
+      <Toaster />
+      <div
+        id="container"
+        className="bg-opacity-30 backdrop-blur-lg fixed inset-0 flex justify-center z-20 py-10"
+        onClick={closeModal}
+      >
+        <div className="max-w-2xl rounded-xl p-10 relative shad h-max">
+          <h1 className="text-2xl font-bold mb-10">Create a new Todo</h1>
+          <button
+            className=" text-red-500 absolute top-2 right-2 bg-red-200 p-2 rounded-full hover:bg-red-300"
+            onClick={() => setModal((m) => !m)}
+          >
+            <VscChromeClose className="text-xl" />
+          </button>
+          <div>
+            <form>
+              <input
+                type="text"
+                placeholder="Title"
+                className="p-2 bg-slate-200 w-96"
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+              <br />
+              <textarea
+                type="text"
+                placeholder="Description"
+                className="p-2 bg-slate-200 my-5 w-96"
+                onChange={(e) => setDescription(e.target.value)}
+                required
+              />
+              <br />
+              <div>
+                <h4 className="text-lg font-bold mb-2">Status</h4>
+                <select
+                  value={selectedOption ? selectedOption : "Select an option"}
+                  onChange={handleChange}
+                  className="px-20 py-2 border border-gray-400 rounded"
+                >
+                  {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <h4 className="text-lg font-bold mb-2 mt-5">Date</h4>
+                <input
+                  type="date"
+                  className="w-96 p-2 rounded shad"
+                  required
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </div>
+              <br />
+              <button
+                className="bg-blue-500 px-10 text-white p-2 w-96 rounded"
+                onClick={createTodo}
+                type="submit"
+              >
+                Create
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,4 +1,5 @@
 import React from "react";
+import toast from "react-hot-toast";
 import { CiEdit } from "react-icons/ci";
 import { VscTrash } from "react-icons/vsc";
 import { useDeleteTaskMutation } from "../redux/slices/apiSlice";
@@ -8,37 +9,43 @@ export default function Task({ task }) {
   const handleDelete = async () => {
     const res = await deleteTask(task._id);
     console.log(res);
+    if (res.data.deletedCount === 1) {
+      toast.success("Task Deleted Successfully");
+    }
   };
   return (
-    <div className="p-3 shad my-2 h-52 rounded flex flex-col justify-between lg:flex-row lg:items-center gap-x-5 2xl:gap-x-10 relative lg:h-20 w-full">
-      <p className="truncate lg:w-40 text-lg font-bold">{task.title}</p>
+    <>
+      <div className="p-3 shad my-2 h-52 rounded flex flex-col justify-between lg:flex-row lg:items-center gap-x-5 2xl:gap-x-10 relative lg:h-20 w-full">
+        <p className="truncate lg:w-40 text-lg font-bold">{task.title}</p>
 
-      <p className="lg:w-96 truncate ">{task.description}</p>
-      <div className="flex items-center">
-        <span className="text-sm">Created At :</span>
-        <span className="text-gray-400 ml-2 text-xs font-semibold">
-          {task.createdAt.substring(0, 10)}
-        </span>
-      </div>
+        <p className="lg:w-96 truncate ">{task.description}</p>
+        <div className="flex items-center">
+          <span className="text-sm">Created At :</span>
+          <span className="text-gray-400 ml-2 text-xs font-semibold">
+            {task.createdAt.substring(0, 10)}
+          </span>
+        </div>
 
-      <div className="flex items-center">
-        <span className="text-sm">Pending At :</span>{" "}
-        <span className="text-gray-400 ml-2 text-xs font-semibold mr-28 ">
-          {task.date}
-        </span>
-      </div>
+        <div className="flex items-center">
+          <span className="text-sm">Pending At :</span>{" "}
+          <span className="text-gray-400 ml-2 text-xs font-semibold mr-28 ">
+            {task.date}
+          </span>
+        </div>
 
-      <div className="lg:absolute top-14 lg:top-5 right-0">
-        <button
-          className="lg:px-5 py-2 text-red-600 mr-5 lg:mr-0"
-          onClick={handleDelete}
-        >
-          <VscTrash size={20} />
-        </button>
-        <button className="lg:px-5 py-2 text-green-600">
-          <CiEdit size={20} />
-        </button>
+        <div className="lg:absolute top-14 lg:top-5 right-0">
+          <button
+            className="lg:px-5 py-2 text-red-600 mr-5 lg:mr-0"
+            onClick={handleDelete}
+          >
+            <VscTrash size={20} />
+          </button>
+          <button className="lg:px-5 py-2 text-green-600">
+            <CiEdit size={20} />
+          </button>
+        </div>
       </div>
-    </div>
+      <div></div>
+    </>
   );
 }
